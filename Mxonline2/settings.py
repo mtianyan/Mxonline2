ecb60778253e05150b726a66384920405ecca52c
mtianyan <1147727180@qq.com>
2018-01-09 06:29:01 +0800

    admin中添加管理器&注册。时区,语言,utc(False).数据库中选项参数。female的长度修改, createsuperuser.对应5-1

--- Mxonline2/settings.py
+++ Mxonline2/settings.py
@@ -89,9 +89,10 @@ DATABASES = {
         'NAME': 'mxonline2',
         'USER': 'root',
         'PASSWORD': 'tp158917',
-        'HOST':'127.0.0.1'
+        'HOST':'127.0.0.1',
 
-    }
+    },
+
 }
 
 
@@ -117,18 +118,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# 语言改为中文
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+# 时区改为上海
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# 数据库存储使用时间，True时间会被存为UTC的时间
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
