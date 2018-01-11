1d05deb2c68b807699da1a0fa3965d6eb51b8af2
mtianyan <1147727180@qq.com>
2018-01-12 03:14:29 +0800

    7-6 完成列表筛选功能以及排序功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -14,10 +14,42 @@ class OrgView(View):
     def get(self,request):
         # 查找到所有的课程机构
         all_orgs = CourseOrg.objects.all()
-        # 总共有多少家机构使用count进行统计
-        org_nums = all_orgs.count()
+
+        # 热门机构,如果不加负号会是有小到大。
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
+
         # 取出所有的城市
         all_city = CityDict.objects.all()
+
+        # 取出筛选的城市,默认值为空
+        city_id = request.GET.get('city', "")
+        # 如果选择了某个城市,也就是前端传过来了值
+        if city_id:
+            # 外键city在数据中叫city_id
+            # 我们就在机构中作进一步筛选
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', "")
+        if category:
+            # 我们就在机构中作进一步筛选类别
+            all_orgs = all_orgs.filter(category=category)
+
+        # 进行排序
+        sort = request.GET.get('sort', "")
+        if sort:
+            if sort == "students":
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == "courses":
+                all_orgs = all_orgs.order_by("-course_nums")
+
+
+
+
+
+
+        # 总共有多少家机构使用count进行统计
+        org_nums = all_orgs.count()
         # 对课程机构进行分页
         # 尝试获取前台get请求传递过来的page参数
         # 如果是不合法的配置参数默认返回第一页
@@ -33,4 +65,8 @@ class OrgView(View):
             "all_orgs":orgs,
             "all_city": all_city,
             "org_nums": org_nums,
+            "city_id":city_id,
+            "category":category,
+            "hot_orgs":hot_orgs,
+            "sort": sort,
         })
